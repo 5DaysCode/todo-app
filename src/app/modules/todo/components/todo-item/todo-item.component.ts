666f38395fe5a27215313176b0d13c87c00978ee
5DaysCode <5DaysCode@gmail.com>
2023-03-31 14:05:23 +0200

    added some changes to todo-list

--- src/app/modules/todo/components/todo-item/todo-item.component.ts
+++ src/app/modules/todo/components/todo-item/todo-item.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { ToDo } from '../../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo!: ToDo;
+  @Output() toggle = new EventEmitter<ToDo>();
+  @Output() remove = new EventEmitter<ToDo>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onToggle() {
+    this.toggle.emit(this.todo);
   }
 
+  onRemove() {
+    this.remove.emit(this.todo);
+  }
 }
--- src/app/modules/todo/components/todo-list/todo-list.component.ts
+++ src/app/modules/todo/components/todo-list/todo-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ToDo } from '../../models/todo.model';
+import { ToDoService } from '../../services/todo-service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  todos$: Observable<ToDo[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private todoService: ToDoService) {
+    this.todos$ = this.todoService.todos;
   }
 
+  toggle(todo: ToDo) {
+    this.todoService.toggleTodo(todo);
+  }
+
+  remove(todo: ToDo) {
+    this.todoService.removeTodo(todo);
+  }
+
+  ngOnInit(): void {}
 }
