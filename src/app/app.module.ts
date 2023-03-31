9df911db9d6c4cc188be29a80471a6ce96e9078c
5DaysCode <5DaysCode@gmail.com>
2023-03-31 12:57:50 +0200

    added some minior changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './modules/todo/components/todo-list/todo-list.component';
 import { TodoItemComponent } from './modules/todo/components/todo-item/todo-item.component';
 import { TodoInputComponent } from './modules/todo/components/todo-input/todo-input.component';
+import { FormsModule } from '@angular/forms';
+import { ToDoService } from './modules/todo/services/todo-service';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     TodoItemComponent,
-    TodoInputComponent
+    TodoInputComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  providers: [ToDoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/todo/components/todo-input/todo-input.component.ts
+++ src/app/modules/todo/components/todo-input/todo-input.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoService } from '../../services/todo-service';
 
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
-  styleUrls: ['./todo-input.component.scss']
+  styleUrls: ['./todo-input.component.scss'],
 })
 export class TodoInputComponent implements OnInit {
+  title: string = '';
+  constructor(private todoService: ToDoService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addTodo() {
+    if (this.title.trim()) {
+      this.todoService.addTodo(this.title);
+      this.title = '';
+    }
   }
-
 }
